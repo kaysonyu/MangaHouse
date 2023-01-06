7f7cfb15511e7bdd48bc1a21d7c5e8f89cfd9fd1
YuKang <2833721527@qq.com>
2023-01-06 09:03:17 +0800

    界面微调

--- app/src/main/java/com/android/mangahouse/request/ComicSearchService.kt
+++ app/src/main/java/com/android/mangahouse/request/ComicSearchService.kt
@@ -9,16 +9,6 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ComicSearchService {
-
-//    @GET("comic/search/{option}/{key}/1/30")
-//    fun getComicResp(@Path("option") option: String, @Path("key") key:String): Call<ComicResp>
-//
-//    @GET("comicChapter/search/{comicId}")
-//    fun getComicChapterResp(@Path("comicId") comicId: String): Call<ComicChapterResp>
-//
-//    @GET("comicContent/search/{chapterId}")
-//    fun getComicContentResp(@Path("chapterId") chapterId: String): Call<ComicContentResp>
-
     @GET("/aggregate/search")
     fun getComicResp(@Query("name") name:String, @Query("site") site:String): Call<ComicResp>
 
